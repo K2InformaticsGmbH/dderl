9b6797dc756b8d9cb64a94545953be8d00803ee2
Bikram Chatterjee <razorpeak@gmail.com>
2012-07-05 21:08:06 +0200

    Added detached starAdded detached start

--- start.sh
+++ start.sh
@@ -2,3 +2,4 @@
 cd `dirname $0`
 #exec erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s reloader -s dderl
 exec erl -pa $PWD/ebin $PWD/deps/*/ebin -s reloader -s dderl
+#exec erl -pa $PWD/ebin $PWD/deps/*/ebin -s reloader -s dderl -detached
