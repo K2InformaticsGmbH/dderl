e722ad4d03f14e3c3359310da12c522980a93778
Bikram Chatterjee <razorpeak@gmail.com>
2012-08-30 10:21:25 +0200

    Fixed start and config scripts

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cd `dirname $0`
 #exec erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s reloader -s dderl
-exec werl.exe -pa $PWD/ebin -pa $PWD/deps/*/ebin -name dderl@oltapmop-sbsm01.it.bwns.ch -setcookie dderl -s reloader -s dderl
-#exec erl -pa $PWD/ebin $PWD/deps/*/ebin -s reloader -s dderl -detached
-#exec erl.exe -pa $PWD/ebin -pa $PWD/deps/*/ebin -name dderl@oltapmop-sbsm01.it.bwns.ch -setcookie dderl -s reloader -s dderl -detached
+HOST=`hostname`
+exec werl.exe -pa $PWD/ebin -pa $PWD/deps/*/ebin -name dderl@$HOST.it.bwns.ch -setcookie dderl -s reloader -s dderl
+#exec werl.exe -pa $PWD/ebin -pa $PWD/deps/*/ebin -name dderl@$HOST.it.bwns.ch -setcookie dderl -s reloader -s dderl -detached
+#exec werl.exe -pa $PWD/ebin -pa $PWD/deps/*/ebin -name dderl@localhost -setcookie dderl -s reloader -s dderl
