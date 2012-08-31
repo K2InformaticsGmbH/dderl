7b85289b658d546ae0c71e250a051667ad1bf138
Bikram Chatterjee <razorpeak@gmail.com>
2012-08-31 10:39:37 +0200

    srevice installer script fixed

--- install.bat
+++ install.bat
@@ -1,5 +1,17 @@
+@ECHO OFF
 FOR /F "usebackq" %%i IN (`hostname`) DO SET HOST=%%i
-::echo "-kernel error_logger {file,\\""D:/Projects/Git/dderl/kernel.txt\\""} -pa D:/Projects/Git/dderl/ebin -pa D:/Projects/Git/dderl/deps/erloci/ebin -pa D:/Projects/Git/dderl/deps/imem/ebin -pa D:/Projects/Git/dderl/deps/mochiweb/ebin -pa D:/Projects/Git/dderl/deps/sqlparse/ebin -pa D:/Projects/Git/dderl/deps/webmachine/ebin -name dderl@%HOST%.it.bwns.ch -setcookie dderl -s reloader -s dderl"
+
+SET NODE=dderl@%HOST%.it.bwns.ch
+
+SET BASE=D:/Projects/Git/dderl
+SET PAs= -pa %BASE%/ebin
+SET PAs=%PAs% -pa ./deps/imem/ebin
+SET PAs=%PAs% -pa ./deps/erloci/ebin
+SET PAs=%PAs% -pa ./deps/sqlparse/ebin
+SET PAs=%PAs% -pa ./deps/mochiweb/ebin
+SET PAs=%PAs% -pa ./deps/webmachine/ebin
+
 erlsrv remove "dderl"
-erlsrv add "dderl" -stopaction "init:stop()." -debugtype reuse -args "-kernel error_logger {file,\\""D:/Projects/Git/dderl/kernel.txt\\""} -pa D:/Projects/Git/dderl/ebin -pa D:/Projects/Git/dderl/deps/erloci/ebin -pa D:/Projects/Git/dderl/deps/imem/ebin -pa D:/Projects/Git/dderl/deps/mochiweb/ebin -pa D:/Projects/Git/dderl/deps/sqlparse/ebin -pa D:/Projects/Git/dderl/deps/webmachine/ebin -name dderl@%HOST%.it.bwns.ch -setcookie dderl -s reloader -s dderl"
+erlsrv add "dderl" -stopaction "init:stop()." -workdir %BASE% -name %NODE% -debugtype reuse -args "-kernel error_logger {file,\\""D:/Projects/Git/dderl/kernel.txt\\""} %PAs% -setcookie dderl -s reloader -s dderl"
 pause
+
