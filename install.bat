0995ebe0f3881add0156bef8042d22acd9c666c8
Bikram Chatterjee <Bikram Chatterjee>
2012-08-31 09:19:33 +0200

    service starter

--- install.bat
+++ install.bat
@@ -1,3 +1,5 @@
+FOR /F "usebackq" %%i IN (`hostname`) DO SET HOST=%%i
+::echo "-kernel error_logger {file,\\""D:/Projects/Git/dderl/kernel.txt\\""} -pa D:/Projects/Git/dderl/ebin -pa D:/Projects/Git/dderl/deps/erloci/ebin -pa D:/Projects/Git/dderl/deps/imem/ebin -pa D:/Projects/Git/dderl/deps/mochiweb/ebin -pa D:/Projects/Git/dderl/deps/sqlparse/ebin -pa D:/Projects/Git/dderl/deps/webmachine/ebin -name dderl@%HOST%.it.bwns.ch -setcookie dderl -s reloader -s dderl"
 erlsrv remove "dderl"
-erlsrv add "dderl" -stopaction "init:stop()." -debugtype reuse -args "-kernel error_logger {file,\\""D:/Projects/Git/dderl/kernel.txt\\""} -pa D:/Projects/Git/dderl/ebin -pa D:/Projects/Git/dderl/deps/*/ebin -name dderl@oltapmop-sbsm01.it.bwns.ch -setcookie dderl -s reloader -s dderl"
+erlsrv add "dderl" -stopaction "init:stop()." -debugtype reuse -args "-kernel error_logger {file,\\""D:/Projects/Git/dderl/kernel.txt\\""} -pa D:/Projects/Git/dderl/ebin -pa D:/Projects/Git/dderl/deps/erloci/ebin -pa D:/Projects/Git/dderl/deps/imem/ebin -pa D:/Projects/Git/dderl/deps/mochiweb/ebin -pa D:/Projects/Git/dderl/deps/sqlparse/ebin -pa D:/Projects/Git/dderl/deps/webmachine/ebin -name dderl@%HOST%.it.bwns.ch -setcookie dderl -s reloader -s dderl"
 pause
