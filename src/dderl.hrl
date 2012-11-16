f03ee7d46be40a15a7d264eade0ca4c4712f3264
Stefan Ochsenbein <ochsenbein@bluewin.ch>
2012-11-16 13:32:07 +0100

    dderl.hrl replaced

--- src/dderl.hrl
+++ src/dderl.hrl
@@ -1,54 +1,54 @@
--record(accounts, { user
-                  , password
-                  , db_connections
-                  , db_files
-                  }
-       ).
-
--record(db_connection, { name = ""
-                       , adapter
-                       , ip
-                       , port
-                       , service
-                       , type
-                       , user
-                       , password
-                       , tns
-                       }
-       ).
-
--record(common,   { adapter
-                  , db_files
-                  }
-       ).
-
--record(file,     {name = ""
-                  , content = ""
-                  , posX = -1
-                  , posY = -1
-                  , width = -1
-                  , height = -1
-                  }
-       ).
 
 -define(DEFAULT_ROW_SIZE, 100).
--define(PASSWORD_VALIDITY, 60).              %% days
 
-%% rr("src/dderl.hrl").
-%% F = fun(User) ->
-%%     [Acc|_]=imem_if:read(accounts, User),
-%%     {struct, ConsList} = mochijson:decode(Acc#accounts.db_connections),
-%%     NewAcc = Acc#accounts{db_connections =
-%%         [#db_connection {
-%%             name       = N
-%%             , adapter  = proplists:get_value("adapter", P, "")
-%%             , ip       = proplists:get_value("ip", P, "")
-%%             , port     = list_to_integer(proplists:get_value("port", P, "0"))
-%%             , service  = proplists:get_value("service", P, "")
-%%             , type     = proplists:get_value("type", P, "")
-%%             , user     = proplists:get_value("user", P, "")
-%%             , password = proplists:get_value("password", P, "")
-%%         } || {N, {struct, P}} <- ConsList]},
-%%     imem_if:insert_into_table(accounts, NewAcc)
-%% end.
+-record(ddAdapter,                          %% DDerl adapter (connect to databases)              
+                  { id                      :: atom()             %% oci | imem | ets | os_text | dfs_text | hdfs_text
+                  , fullName                :: string()           %% displayed in drop down box
+                  }
+       ). 
 
+-record(ddInterface,                        %% DDerl client interface (connect to ui / applications)               
+                  { id                      :: atom()             %% ddjson
+                  , fullName                :: string()           %% displayed in drop down box
+                  }
+       ).
+
+-record(ddConn,                             %% DB connection    
+                  { id                      ::ddEntityId()       
+                  , name                    ::binary()          %% connection name (mutable)
+                  , owner                   ::ddEntityId()      %% account.id of creator / owner
+                  , adapter                 ::atom()            %% oci | imem | ets | os_text | dfs_text | hdfs_text
+                  , access                  ::any()             %% erlang term depending on adapter (e.g. ip+service or tns)
+                  , schema                  ::any()             %% erlang term depending on adapter (e.g. name or uri or data root path)
+                  }
+       ).
+      
+-record(ddCmd,                              %% DB command     
+                  { id                      ::ddEntityId()       
+                  , name                    ::binary()          %% command template name (mutable)
+                  , owner                   ::ddEntityId()      %% account.id of creator / owner
+                  , adapters                ::[atom()]          %% can be used for this list of ddAdap
+                  , conns                   ::[ddEntityId()]    %% can be used for this list of dbConn references
+                  , command                 ::string()          %% erlang term depending on adapter (e.g. SQL text)
+                  , opts                    ::any()             %% command options ()
+                  }
+       ).
+
+-record(ddView,                             %% user representation of a db command including rendering parameters
+                  { id                      ::ddEntityId()
+                  , interface               ::atom()            %% interface plugin (ddjson for now)  
+                  , owner                   ::ddEntityId()      %% account.id of creator / owner
+                  , name                    ::binary()          %% should default to command name
+                  , cmd                     ::ddEntityId()      %% db command id
+                  , state                   ::any()             %% transparent viewstate (managed by client application)
+                  }
+       ).
+
+-record(ddDash,                             %% user representation of a dashboard (collection of views)
+                  { id                      ::ddEntityId()
+                  , interface               ::atom()            %% interface plugin (ddjson for now)  
+                  , owner                   ::ddEntityId()      %% account.id of creator / owner
+                  , name                    ::binary()          %% should default to command name
+                  , views                   ::[ddEntityId()]    %% ddView.ids
+                  }
+       ).
