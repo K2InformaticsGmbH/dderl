a4fe1bfaec800b30b5a33acee46b9a8d7f80717f
Bikram Chatterjee <razorpeak@gmail.com>
2012-12-05 02:06:09 +0100

    working till browse data of table

--- src/imem_adapter.erl
+++ src/imem_adapter.erl
@@ -13,9 +13,13 @@ init() ->
                                  , adapter = imem
                                  , access = [{ip, "local"}, {user, "admin"}]
                                  }),
-    dderl_dal:add_command(imem, "All Tables", "select qname from all_tables", [{rowfun, fun([I,{_,F}|R]) ->
-                                                                                            [I,F|R]
-                                                                                        end}]).
+    dderl_dal:add_command(imem, "All Tables", "select qname from all_tables"
+                                            , [{rowfun, fun([I,{_,F}|R]) -> [I,F|R] end}]).
+
+-record(priv, { sess
+              , stmts
+              , row_fun
+       }).
 
 process_cmd({"connect", BodyJson}, SrvPid, _) ->
     Schema = binary_to_list(proplists:get_value(<<"service">>, BodyJson, <<>>)),
@@ -40,8 +44,8 @@ process_cmd({"connect", BodyJson}, SrvPid, _) ->
     io:format(user, "Session ~p~n", [Session]),
     dderl_session:log(SrvPid, "Connected to Params ~p~n", [{Type, Opts}]),
     Statements = [],
-    {{Session, Statements}, "{\"connect\":\"ok\"}"};
-process_cmd({"query", BodyJson}, SrvPid, {Session,Statements}) ->
+    {#priv{sess=Session, stmts=Statements}, "{\"connect\":\"ok\"}"};
+process_cmd({"query", BodyJson}, SrvPid, #priv{sess=Session, stmts=Statements} = Priv) ->
     Query = binary_to_list(proplists:get_value(<<"qstr">>, BodyJson, <<>>)),
     Id = proplists:get_value(<<"id">>, BodyJson, <<>>),
     Cmd = dderl_dal:get_command(Id),
@@ -52,7 +56,7 @@ process_cmd({"query", BodyJson}, SrvPid, {Session,Statements}) ->
     ParseTree = [],
     io:format(user, "query ~p~n", [Query]),
     dderl_session:log(SrvPid, "[~p] Query ~p~n", [SrvPid, {Session, Query}]),
-    case Session:exec(Query, ?DEFAULT_ROW_SIZE, Fun) of
+    case Session:exec(Query, ?DEFAULT_ROW_SIZE) of
         {ok, Clms, Statement} ->
             StmtHndl = erlang:phash2(Statement),
             io:format(user, "Clms ~p~n", [Clms]),
@@ -60,55 +64,55 @@ process_cmd({"query", BodyJson}, SrvPid, {Session,Statements}) ->
             Statement:start_async_read(),
             Resp = "{\"headers\":"++gen_adapter:string_list_to_json(Columns, [])++
             ",\"statement\":"++integer_to_list(StmtHndl)++"}",
-            {{Session,[{StmtHndl, {Statement, Query, ParseTree}}|Statements]}, Resp};
+            {Priv#priv{stmts=[{StmtHndl, {Statement, Query, ParseTree}}|Statements], row_fun=Fun}, Resp};
         {error, Error} ->
             io:format(user, "query error ~p~n", [Error]),
             dderl_session:log(SrvPid, "[~p] Query Error ~p~n", [SrvPid, Error]),
             Resp = "{\"headers\":[],\"statement\":0,\"error\":\""++Error++"\"}",
-            {{Session,Statements}, Resp}
+            {Priv, Resp}
     end;
-process_cmd({"row_prev", BodyJson}, SrvPid, {_,Statements} = MPort) ->
+process_cmd({"row_prev", BodyJson}, SrvPid, #priv{stmts=Statements, row_fun=Fun} = Priv) ->
     StmtKey = proplists:get_value(<<"statement">>, BodyJson, <<>>),
     case proplists:get_value(StmtKey, Statements) of
         undefined ->
             dderl_session:log(SrvPid, "[~p, ~p] Statements ~p~n", [SrvPid, StmtKey, Statements]),
-            {MPort, "{\"rows\":[]}"};
-        {Statement, _, _} -> {MPort, gen_adapter:prepare_json_rows(prev, -1, Statement, StmtKey, SrvPid)}
+            {Priv, "{\"rows\":[]}"};
+        {Statement, _, _} -> {Priv, gen_adapter:prepare_json_rows(prev, -1, Statement, StmtKey, SrvPid, Fun)}
     end;
-process_cmd({"row_next", BodyJson}, SrvPid, {_,Statements} = MPort) ->
+process_cmd({"row_next", BodyJson}, SrvPid, #priv{stmts=Statements, row_fun=Fun} = Priv) ->
     StmtKey = proplists:get_value(<<"statement">>, BodyJson, <<>>),
     RowNum = proplists:get_value(<<"row_num">>, BodyJson, -1),
     case proplists:get_value(StmtKey, Statements) of
         undefined ->
             dderl_session:log(SrvPid, "[~p, ~p] Statements ~p~n", [SrvPid, StmtKey, Statements]),
-            {MPort, "{\"rows\":[]}"};
-        {Statement, _, _} -> {MPort, gen_adapter:prepare_json_rows(next, RowNum, Statement, StmtKey, SrvPid)}
+            {Priv, "{\"rows\":[]}"};
+        {Statement, _, _} -> {Priv, gen_adapter:prepare_json_rows(next, RowNum, Statement, StmtKey, SrvPid, Fun)}
     end;
-process_cmd({"stmt_close", BodyJson}, SrvPid, {Session,Statements} = MPort) ->
+process_cmd({"stmt_close", BodyJson}, SrvPid, #priv{stmts=Statements} = Priv) ->
     StmtKey = proplists:get_value(<<"statement">>, BodyJson, <<>>),
     case proplists:get_value(StmtKey, Statements) of
         undefined ->
             dderl_session:log(SrvPid, "[~p] Statement ~p not found. Statements ~p~n", [SrvPid, StmtKey, proplists:get_keys(Statements)]),
-            {MPort, "{\"rows\":[]}"};
+            {Priv, "{\"rows\":[]}"};
         {Statement, _, _} ->
             dderl_session:log(SrvPid, "[~p, ~p] Remove statement ~p~n", [SrvPid, StmtKey, Statement]),
             Statement:close(),
             {_,NewStatements} = proplists:split(Statements, [StmtKey]),
-            {{Session,NewStatements}, "{\"rows\":[]}"}
+            {Priv#priv{stmts=NewStatements}, "{\"rows\":[]}"}
     end;
-process_cmd({"get_buffer_max", BodyJson}, SrvPid, {_,Statements} = MPort) ->
+process_cmd({"get_buffer_max", BodyJson}, SrvPid, #priv{stmts=Statements} = Priv) ->
     StmtKey = proplists:get_value(<<"statement">>, BodyJson, <<>>),
     case proplists:get_value(StmtKey, Statements) of
         undefined ->
             dderl_session:log(SrvPid, "[~p] Statement ~p not found. Statements ~p~n", [SrvPid, StmtKey, proplists:get_keys(Statements)]),
-            {MPort, "-1"};
+            {Priv, "-1"};
         {Statement, _, _} ->
             {ok, Finished, CacheSize} = Statement:get_buffer_max(),
             dderl_session:log(SrvPid, "[~p, ~p] get_buffer_max ~p Fînished ~p~n", [SrvPid, StmtKey, CacheSize, Finished]),
-            {MPort, "{\"count\":"++integer_to_list(CacheSize)++", \"finished\":"++atom_to_list(Finished)++"}"}
+            {Priv, "{\"count\":"++integer_to_list(CacheSize)++", \"finished\":"++atom_to_list(Finished)++"}"}
     end;
-process_cmd({"get_query", BodyJson}, SrvPid, MPort) -> gen_adapter:process_cmd({"get_query", BodyJson}, SrvPid, MPort);
-process_cmd({"parse_stmt", BodyJson}, SrvPid, MPort) -> gen_adapter:process_cmd({"parse_stmt", BodyJson}, SrvPid, MPort);
-process_cmd({Cmd, _BodyJson}, _SrvPid, MPort) ->
+process_cmd({"get_query", BodyJson}, SrvPid, Priv) -> gen_adapter:process_cmd({"get_query", BodyJson}, SrvPid, Priv);
+process_cmd({"parse_stmt", BodyJson}, SrvPid, Priv) -> gen_adapter:process_cmd({"parse_stmt", BodyJson}, SrvPid, Priv);
+process_cmd({Cmd, _BodyJson}, _SrvPid, Priv) ->
     io:format(user, "Cmd ~p~n", [Cmd]),
-    {MPort, "{\"rows\":[]}"}.
+    {Priv, "{\"rows\":[]}"}.
--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-cd `dirname $0`
-#exec erl -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -s reloader -s dderl
-HOST=`hostname`
-#exec werl.exe -pa $PWD/ebin -pa $PWD/deps/*/ebin -name dderl@$HOST.it.bwns.ch -setcookie dderl -s reloader -s dderl
-werl.exe -pa $PWD/ebin -pa $PWD/deps/*/ebin -name dderl@$HOST.it.bwns.ch -setcookie dderl -s reloader -s dderl
-#exec werl.exe -pa $PWD/ebin -pa $PWD/deps/*/ebin -name dderl@$HOST.it.bwns.ch -setcookie dderl -s reloader -s dderl -detached
-#exec werl.exe -pa $PWD/ebin -pa $PWD/deps/*/ebin -name dderl@localhost -setcookie dderl -s reloader -s dderl
+# cd `dirname $0`
+# HOST=`hostname`
+#werl.exe -pa $PWD/ebin -pa $PWD/deps/*/ebin -name dderl@$HOST.it.bwns.ch -setcookie imem -s reloader -s dderl
+#werl.exe -pa $PWD/ebin -pa $PWD/deps/*/ebin -name dderl@127.0.0.1 -setcookie imem -s reloader -s dderl -imem erl_cluster_mgr \'CM@127.0.0.1\'
+werl.exe -pa $PWD/ebin -pa $PWD/deps/*/ebin -name dderl@127.0.0.1 -setcookie imem -s reloader -s dderl
