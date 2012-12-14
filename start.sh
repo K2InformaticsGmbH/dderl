bcea609d112893e77b54a953e331ad68d7143991
Bikram Chatterjee <bikram@localhost.localdomain>
2012-12-14 18:27:53 +0100

    ^M removed

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 # HOST=`hostname`
 #werl.exe -pa $PWD/ebin -pa $PWD/deps/*/ebin -name dderl@$HOST.it.bwns.ch -setcookie imem -s reloader -s dderl
 #werl.exe -pa $PWD/ebin -pa $PWD/deps/*/ebin -name dderl@127.0.0.1 -setcookie imem -s reloader -s dderl -imem erl_cluster_mgr \'CM@127.0.0.1\'
-werl.exe -pa $PWD/ebin -pa $PWD/deps/*/ebin -name dderl@127.0.0.1 -setcookie imem -s reloader -s dderl -imem mnesia_node_type disc
+erl -pa $PWD/ebin -pa $PWD/deps/*/ebin -name dderl@127.0.0.1 -setcookie imem -s reloader -s dderl -imem mnesia_node_type disc
