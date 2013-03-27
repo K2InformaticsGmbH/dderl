220c13479cf48fc96cf9c7b8cca01f13f43c5131
Bikram Chatterjee <razorpeak@gmail.com>
2013-03-27 13:54:31 +0100

    start script for cluster dderl start

--- start_zhh.sh
+++ start_zhh.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
-werl.exe -pa $PWD/ebin -pa $PWD/deps/*/ebin -name dderl@10.66.148.80 -setcookie z6T5r4uF3 -s reloader -s dderl -imem erl_cluster_mgr \'dderl@10.66.220.80\'
+unamestr=`uname`
+exename=werl.exe
+#exename=erl
+if [[ "$unamestr" == 'Linux' ]]; then
+     exename=erl
+fi
+echo "exe $exename unamestr $unamestr"
+if [[ "$unamestr" == 'Linux' ]]; then
+    $exename -pa $PWD/ebin -pa $PWD/deps/*/ebin -name dderl@127.0.0.1 -setcookie imem -s dderl -imem mnesia_node_type disc
+else
+    start //MAX $exename -pa $PWD/ebin -pa $PWD/deps/*/ebin -name dderl@10.66.148.80 -setcookie z6T5r4uF3 -s reloader -s dderl -imem erl_cluster_mgr \'dderl@10.66.220.80\'
+#    $exename -pa $PWD/ebin -pa $PWD/deps/*/ebin -name dderl@10.66.148.80 -setcookie z6T5r4uF3 -s reloader -s dderl -imem erl_cluster_mgr \'dderl@10.66.220.80\'
+fi
