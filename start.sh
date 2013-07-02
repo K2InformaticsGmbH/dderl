34cfc323a90b2508e402a0c865ccbf4bd2926d52
Bikram Chatterjee <razorpeak@gmail.com>
2013-07-02 22:08:22 +0200

    new relase 1.0.3

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ cd apps/dderl
 
 unamestr=`uname`
 if [[ "$unamestr" == 'Linux' ]]; then
-    erl -pa ebin -pa ../../deps/*/ebin -s "dderl" -dderl port 8443 -imem tcp_port 8125
+    erl -pa ebin -pa ../../deps/*/ebin -name dderl@127.0.0.1 -setcookie dderl -s dderl -dderl port 8443 -imem tcp_port 8125
 else
-    start //MAX werl.exe -pa ebin -pa ../../deps/*/ebin -s "dderl" -dderl port 8443 -imem tcp_port 8125
+    start //MAX werl.exe -pa ebin -pa ../../deps/*/ebin -name dderl@127.0.0.1 -setcookie dderl -s dderl -dderl port 8443 -imem tcp_port 8125
 fi
