51e617dde273d1c6ce4ec740feb8b0b970eb1c0f
Bikram Chatterjee <razorpeak@gmail.com>
2013-08-05 10:59:48 +0200

    start script improved

--- start.sh
+++ start.sh
@@ -3,7 +3,9 @@ cd apps/dderl
 
 unamestr=`uname`
 if [[ "$unamestr" == 'Linux' ]]; then
-    erl -pa ebin -pa ../../deps/*/ebin -name dderl@127.0.0.1 -setcookie dderl -s dderl -dderl port 8443 -imem tcp_port 8125
+    exename=erl
 else
-    start //MAX werl.exe -pa ebin -pa ../../deps/*/ebin -name dderl@127.0.0.1 -setcookie dderl -s dderl -dderl port 8443 -imem tcp_port 8125
+    #exename=erl.exe
+    exename='start //MAX werl.exe'
 fi
+$exename -pa ebin -pa ../../deps/*/ebin -name dderl@127.0.0.1 -setcookie dderl -s dderl -dderl port 8443 -imem tcp_port 8125
