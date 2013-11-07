3a092933c961ff70330183d5f6dd95e482a068b3
Bikram Chatterjee <razorpeak@gmail.com>
2013-11-07 10:45:32 +0100

    fixed empty node name

--- start.cmd
+++ start.cmd
@@ -7,9 +7,10 @@ set erlpaths=-pa %CD%/apps/dderl/ebin -pa %deps%
 set kernelconfig=-kernel inet_dist_listen_min 7000 -kernel inet_dist_listen_max 7020 -kernel error_logger {file,\\\""%CD%/log/kernel.txt\\\""}
 set commonparams=%erlpaths% -emu_args -setcookie dderl -dderl port 443 -imem tcp_port 8125 -imem mnesia_schema_name dderlstag -s dderl
 
+set name=-name dderl@%2
+set extra=-imem erl_cluster_mgr 'dderl@%3'
 if not "%~2%~3" == "%~3%~2" (
-    set name=-name dderl@%2
-    set extra=-imem erl_cluster_mgr 'dderl@%3'
+    @echo Cmd '%1' name '%name%' extra '%extra%'
     if "%1" == "add" (
         @echo Adding dderl service
         erlsrv.exe add dderl -c "DDErl Service" -stopaction "init:stop()." -debugtype new -w %CD% %name% -args "%kernelconfig% %commonparams% %extra%"
