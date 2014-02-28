f0e05c6d305bef40294a511a6ec41ff6e0672abf
Agustin Cautin <acautin@gmail.com>
2014-02-28 10:11:43 +0100

    Updated start script to support the creation of a local cluster.

--- start.sh
+++ start.sh
@@ -8,4 +8,4 @@ else
     #exename=erl.exe
     exename='start //MAX werl.exe'
 fi
-$exename -pa ebin -pa ../../deps/*/ebin -name dderl@WKS01$1.k2informatics.ch -setcookie zdss4d2_6tP -dderl port 8443 -imem tcp_port 8125 -imem erl_cluster_mgr \'dderl@WKS01$2.k2informatics.ch\' -imem mnesia_schema_name dderlstag -s dderl 
+$exename -pa ebin -pa ../../deps/*/ebin -name dderl$1@127.0.0.1 -setcookie zdss4d2_6tP -dderl port ${3}443 -imem tcp_port ${3}125 -imem erl_cluster_mgrs [\'dderl$2@127.0.0.1\'] -imem mnesia_schema_name dderlstag -s dderl 
