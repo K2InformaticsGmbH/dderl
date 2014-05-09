db6612f5a627d1cf1241e018d202ca535d4d70af
Stefan Ochsenbein <ochsenbein@bluewin.ch>
2014-05-09 09:39:50 +0200

    redefine start parameters to NodeID ClusterID IMEMPort DDERLPort

--- start.sh
+++ start.sh
@@ -8,4 +8,4 @@ else
     #exename=erl.exe
     exename='start //MAX werl.exe'
 fi
-$exename -pa ebin -pa ../../deps/*/ebin -name dderl$1@127.0.0.1 -setcookie zdss4d2_6tP -dderl port ${3}443 -imem tcp_port ${3}125 -imem erl_cluster_mgrs [\'dderl$2@127.0.0.1\'] -imem mnesia_schema_name dderlstag -s dderl 
+$exename -pa ebin -pa ../../deps/*/ebin -name dderl$1@127.0.0.1 -setcookie zdss4d2_6tP -imem tcp_port $3 -dderl port $4 -imem erl_cluster_mgrs [\'dderl$2@127.0.0.1\'] -imem mnesia_schema_name dderlstag -s dderl 
