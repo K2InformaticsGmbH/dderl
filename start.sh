80c497425e1aad9024636a27dfd880d3a9f39c17
Bikram Chatterjee <razorpeak@gmail.com>
2014-08-08 07:52:54 +0200

    apps/dderl path removed

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-cd apps/dderl
 
 # Parameters:  NodeID ClusterID IMEMPort DDERLPort
 
@@ -10,4 +9,4 @@ else
     #exename=erl.exe
     exename='start //MAX werl.exe'
 fi
-$exename -pa ebin -pa ../../deps/*/ebin -name dderl$1@127.0.0.1 -setcookie zdss4d2_6tP -imem tcp_port $3 -dderl port $4 -imem erl_cluster_mgrs [\'dderl$2@127.0.0.1\'] -imem mnesia_schema_name dderlstag -s dderl 
+$exename -pa ebin -pa deps/*/ebin -name dderl$1@127.0.0.1 -setcookie zdss4d2_6tP -imem tcp_port $3 -dderl port $4 -imem erl_cluster_mgrs [\'dderl$2@127.0.0.1\'] -imem mnesia_schema_name dderlstag -s dderl 
