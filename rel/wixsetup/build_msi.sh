37a2c903d0b2d5aabbebdce8953c3e2b754c43e5
Bikram Chatterjee <razorpeak@gmail.com>
2014-11-06 18:44:55 +0100

    debugging echo

--- rel/wixsetup/build_msi.sh
+++ rel/wixsetup/build_msi.sh
@@ -4,4 +4,5 @@ ScriptDir=$(cd ${0%/*} && pwd)
 ProjDir=$ScriptDir/../../
 Proj=dderl
 ReleaseDir=$ScriptDir
+echo "$ScriptDir $ProjDir $Proj $ReleaseDir"
 $ScriptDir/build_msi.escript $Proj $ProjDir $ReleaseDir $1
