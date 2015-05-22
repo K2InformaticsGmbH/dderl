34cbfb4b65e430f4231151939dffd64aca2a938f
Bikram Chatterjee <razorpeak@gmail.com>
2015-05-22 21:05:50 +0200

    debug logs commented

--- src/dderl_cow_mw.erl
+++ src/dderl_cow_mw.erl
@@ -9,10 +9,10 @@ execute(Req, Env) ->
     {Path, Req} = cowboy_req:path(Req),
     case re:run(Path, "^"++?URLSUFFIX) of
         nomatch ->
-            ?Info("NOT DDErl ~p", [Path]),
+            %?Info("NOT DDErl ~p", [Path]),
             {ok, Req, Env};
         _ ->
             %?Info("Env ~p", [Env]),
-            ?Info("DDErl ~p", [Path]),
+            %?Info("DDErl ~p", [Path]),
             {ok, cowboy_req:set_meta(dderl, true, Req), Env}
     end.
