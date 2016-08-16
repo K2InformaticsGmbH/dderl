72029ba195f831986641d62f6c10edf18b372963
Agustin Cautin <acautin@gmail.com>
2016-08-16 15:32:18 +0200

    Visual improvements to d3 arc-interleaved example - Added nodes to correct left or right side depending on the id prod or stag

--- priv/d3_examples/arc-interleaved.js
+++ priv/d3_examples/arc-interleaved.js
@@ -11,38 +11,55 @@ function init(container, width, height) {
     // Copy and paste the following data in the table:
     /*****
 
-["prod", "platform1"]	{"enabled": true}	IS9VH
-["prod", "platform1", "10.0.0.1"]	{"status":"idle"}	1M1WKR
-["prod", "platform2"]	{"enabled": true}	1PTC7Y
-["prod", "platform3"]	{"enabled": false}	8XMU8
-["prod", "platform3", "10.0.0.1"]	{"status":"idle"}	1U1MLL
-["prod", "platform3", "10.0.0.2"]	{"status":"idle"}	J17TF
-["prod", "platform3", "10.0.0.3"]	{"status":"idle"}	V7JL3
-["prod", "platform4"]	{"enabled": true}	REACA
-["prod", "platform5"]	{"enabled": false}	72Q5E
-["prod", "platform6"]	{"enabled": true}	GQU6N
-["prod", "platform6", "10.0.0.1"]	{"status":"idle"}	22FNZY
-["prod", "platform6", "10.0.0.2"]	{"status":"idle"}	1EUXCI
-["prod", "platform7"]	{"enabled": true}	IS9VH
-["prod", "platform7", "10.0.0.3"]	{"status":"error"}	1M1WKR
-["prod", "platform8"]	{"enabled": true}	1PTC7Y
-["prod", "platform8", "10.0.0.2"]	{"status":"idle"}	8XMU8
-["stag", "platform9"]	{"enabled": true}	1U1MLL
-["stag", "platform9", "192.168.0.2"]	{"status":"idle"}	J17TF
-["stag", "platform6"]	{"enabled": true}	V7JL3
-["stag", "platform6", "192.168.0.1"]	{"status":"idle"}	REACA
-["stag", "platform10"]	{"enabled": true}	72Q5E
-["stag", "platform11"]	{"enabled": true}	GQU6N
-["stag", "platform11", "192.168.0.1"]	{"status":"idle"}	22FNZY
-["stag", "platform11", "192.168.0.2"]	{"status":"idle"}	1EUXCI
+["prod","platform01"]	{"enabled": true}	ZSPGI
+["prod","platform01","10.0.0.1"]	{"status":"idle"}	1UUXWW
+["prod","platform02"]	{"enabled": true}	11TOKT
+["prod","platform03"]	{"enabled": true}	1SF3PY
+["prod","platform03","10.0.0.1"]	{"status":"idle"}	KG4FV
+["prod","platform03","10.0.0.2"]	{"status":"idle"}	TZ1G7
+["prod","platform03","10.0.0.3"]	{"status":"idle"}	22ES62
+["prod","platform04"]	{"enabled": false}	16DMNI
+["prod","platform05"]	{"enabled": false}	13ZI63
+["prod","platform06"]	{"enabled": true}	T6SIK
+["prod","platform06","10.0.0.1"]	{"status":"idle"}	R9WKE
+["prod","platform06","10.0.0.2"]	{"status":"idle"}	1YPVKM
+["prod","platform07"]	{"enabled": true}	21GJH
+["prod","platform07","10.0.0.3"]	{"status":"error"}	1C0VIR
+["prod","platform08"]	{"enabled": true}	1G8553
+["prod","platform08","10.0.0.2"]	{"status":"idle"}	1K5JDG
+["prod","platform09"]	{"enabled": true}	174V1F
+["prod","platform09","10.0.0.1"]	{"status":"idle"}	K6X90
+["prod","platform10"]	{"enabled": true}	203DBM
+["prod","platform10","10.0.0.1"]	{"status": "refreshed"}	XC5I1
+["stag","platform06"]	{"enabled": true}	23SE1K
+["stag","platform06","192.168.0.1"]	{"status":"idle"}	PBG1Y
+["stag","platform11"]	{"enabled": true}	1UQK0V
+["stag","platform11","192.168.0.1"]	{"status":"idle"}	IOW3P
+["stag","platform11","192.168.0.2"]	{"status":"idle"}	1YAQRX
 ["stag","platform12"]	{"enabled": true}	1NOQXI
 ["stag","platform13"]	{"enabled": false}	JPIZY
 ["stag","platform14"]	{"enabled": true}	2FBUU
+["stag","platform14","192.168.0.1"]	{"status": "idle"}	208ASX
 ["stag","platform15"]	{"enabled": false}	1EX091
 ["stag","platform16"]	{"enabled": true}	VORM4
+["stag","platform16","192.168.0.1"]	{"status": "cleaned"}	XYI25
 ["stag","platform17"]	{"enabled": true}	1LVEOW
+["stag","platform17","192.168.0.1"]	{"status": "idle"}	1DEQ20
 
     *****/
+
+    /** Size & positioning parameters */
+    
+    // virtual coordinates drawing arc radius
+    var vArcRadius = 1000;
+    // node radius in virtual coordinates
+    var nradius = 90; // TODO: 90 for 17 nodes seems ok, formula ?
+
+    var centerNodes = [
+        // Position relative to the bottom center after margin.
+        { id: 'stag', position: { x: -1.9 * nradius, y: 0.3 * nradius }, status: 'idle' },
+        { id: 'prod', position: { x: 0, y: -nradius }, status: 'idle' }
+    ];
 
     /** Helper functions for data extraction */
     var getKey = function(row) {
@@ -55,7 +72,8 @@ function init(container, width, height) {
 
     var extractLinksNodes = function(rows) {
         var links = [];
-        var nodes = [];
+        var n0 = [];
+        var n1 = [];
         var nlset = new Set();
         rows.forEach(function(row) {
             var key = getKey(row);
@@ -78,14 +96,21 @@ function init(container, width, height) {
             } else if(key.length === 2) {
                 if(!nlset.has(key[1])) {
                     nlset.add(key[1]);
-                    nodes.push({
-                        id: key[1],
-                        enabled: value.enabled
-                    });
+                    if(key[0] === centerNodes[0].id) {
+                        n0.push({
+                            id: key[1],
+                            enabled: value.enabled
+                        });
+                    } else if(key[0] === centerNodes[1].id) {
+                        n1.push({
+                            id: key[1],
+                            enabled: value.enabled
+                        });
+                    }
                 }
             }
         });
-        return { links: links, nodes: nodes };
+        return { links: links, nodes: n0.concat(n1) };
     };
     /** End data extraction functions */
 
@@ -101,11 +126,6 @@ function init(container, width, height) {
         refreshed: 'purple',
         stopped: 'red'
     };
-
-    // virtual coordinates drawing arc radius
-    var vArcRadius = 1000;
-    // node radius in virtual coordinates
-    var nradius = 100;
     // To see the complete circle when drawing negative coordinates
     // and width and height for the virtual coordinates
     var vBox = {
@@ -114,12 +134,6 @@ function init(container, width, height) {
         w: vArcRadius * 2 + nradius * 2,
         h: vArcRadius + 3 * nradius
     };
-
-    var centerNodes = [
-        // Position relative to the bottom center after margin.
-        { id: 'prod', position: {x: 0, y: 0}, status: 'idle' },
-        { id: 'stag', position: {x: -4 * nradius, y: nradius}, status: 'idle' }
-    ];
 
     var svg = container
         .append('svg')
@@ -230,11 +244,11 @@ function init(container, width, height) {
                 .transition()
                 .attr('cx', function(d, i) {
                     // Interleave smaller radius to allow more nodes.
-                    var r = vArcRadius - 3 * nradius * (i % 2);
+                    var r = vArcRadius - 2 * nradius * (i % 2);
                     return -r * Math.cos((i + 1) * angle);
                 })
                 .attr('cy', function(d, i) {
-                    var r = vArcRadius - 3 * nradius * (i % 2);
+                    var r = vArcRadius - 2 * nradius * (i % 2);
                     return -r * Math.sin((i + 1) * angle);
                 })
                 .style('fill', function(d) {
@@ -247,7 +261,7 @@ function init(container, width, height) {
                 })
                 .enter()
                 .insert('line', 'circle')
-                .attr('stroke-width', 10)
+                .attr('stroke-width', 6)
                 .attr('id', function(d) {
                     return d.id;
                 })
