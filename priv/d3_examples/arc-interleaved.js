5c8fd039d253b3dbe8519a4f30949f43dd68fc56
Agustin Cautin <acautin@gmail.com>
2016-08-30 11:01:57 +0200

    Added support to arrows with the state of the nodes to the arc_example.

--- priv/d3_examples/arc-interleaved.js
+++ priv/d3_examples/arc-interleaved.js
@@ -11,41 +11,40 @@ function init(container, width, height) {
     // Copy and paste the following data in the table:
     /*****
 
-["prod","job01"]	{"platform": "platform01", "args": {"group": "03"}, "enabled": true}	ZSPGI
+["prod","job01"]	{"platform": "platform01", "direction":"pull", "args": {"group": "03"}, "enabled": true}	ZSPGI
 ["prod","job01","10.0.0.1"]	{"status":"idle"}	1UUXWW
-["prod","job02"]	{"platform": "platform02", "args": {"group":"03"}, "enabled": true}	11TOKT
-["prod","job03"]	{"platform": "platform02", "args": {"group":"03"}, "enabled": true}	1SF3PY
+["prod","job02"]	{"platform": "platform02", "direction":"pull", "args": {"group":"03"}, "enabled": true}	11TOKT
+["prod","job03"]	{"platform": "platform02", "direction":"pull", "args": {"group":"03"}, "enabled": true}	1SF3PY
 ["prod","job03","10.0.0.1"]	{"status":"idle"}	KG4FV
 ["prod","job03","10.0.0.2"]	{"status":"idle"}	TZ1G7
 ["prod","job03","10.0.0.3"]	{"status":"idle"}	22ES62
-["prod","job04"]	{"platform": "platform04", "args": {"group":"03"}, "enabled": false}	16DMNI
-["prod","job05"]	{"platform": "platform05", "args": {"group":"02"}, "enabled": false}	13ZI63
-["prod","job06"]	{"platform": "platform06", "args": {"group":"01"}, "enabled": true}	T6SIK
+["prod","job04"]	{"platform": "platform04", "direction":"pull", "args": {"group":"03"}, "enabled": false}	16DMNI
+["prod","job05"]	{"platform": "platform05", "direction":"pull", "args": {"group":"02"}, "enabled": false}	13ZI63
+["prod","job06"]	{"platform": "platform06", "direction":"pull", "args": {"group":"01"}, "enabled": true}	T6SIK
 ["prod","job06","10.0.0.1"]	{"status":"idle"}	R9WKE
 ["prod","job06","10.0.0.2"]	{"status":"idle"}	1YPVKM
-["prod","job07"]	{"platform": "platform07", "args": {"group":"02"}, "enabled": true}	21GJH
+["prod","job07"]	{"platform": "platform07", "direction":"pull", "args": {"group":"02"}, "enabled": true}	21GJH
 ["prod","job07","10.0.0.3"]	{"status":"error"}	1C0VIR
-["prod","job08"]	{"platform": "platform08", "args": {"group":"03"}, "enabled": true}	1G8553
+["prod","job08"]	{"platform": "platform08", "direction":"pull", "args": {"group":"03"}, "enabled": true}	1G8553
 ["prod","job08","10.0.0.2"]	{"status":"idle"}	1K5JDG
-["prod","job09"]	{"platform": "platform09", "args": {"group":"02"}, "enabled": true}	174V1F
+["prod","job09"]	{"platform": "platform09", "direction":"push", "args": {"group":"02"}, "enabled": true}	174V1F
 ["prod","job09","10.0.0.1"]	{"status":"idle"}	K6X90
-["prod","job10"]	{"platform": "platform10", "args": {"group":"02"}, "enabled": true}	203DBM
+["prod","job10"]	{"platform": "platform10", "direction":"pull", "args": {"group":"02"}, "enabled": true}	203DBM
 ["prod","job10","10.0.0.1"]	{"status": "refreshed"}	XC5I1
-["stag","job11"]	{"platform": "platform11", "args": {"group":"01"}, "enabled": true}	1UQK0V
+["stag","job11"]	{"platform": "platform11", "direction":"pull", "args": {"group":"01"}, "enabled": true}	1UQK0V
 ["stag","job11","192.168.0.1"]	{"status":"idle"}	IOW3P
 ["stag","job11","192.168.0.2"]	{"status":"idle"}	1YAQRX
-["stag","job12"]	{"platform": "platform12", "args": {"group":"01"}, "enabled": true}	1NOQXI
-["stag","job13"]	{"platform": "platform13", "args": {"group":"01"}, "enabled": false}	JPIZY
-["stag","job14"]	{"platform": "platform14", "args": {"group":"01"}, "enabled": true}	2FBUU
+["stag","job12"]	{"platform": "platform12", "direction":"pull", "args": {"group":"01"}, "enabled": true}	1NOQXI
+["stag","job13"]	{"platform": "platform13", "direction":"pull", "args": {"group":"01"}, "enabled": false}	JPIZY
+["stag","job14"]	{"platform": "platform14", "direction":"pull", "args": {"group":"01"}, "enabled": true}	2FBUU
 ["stag","job14","192.168.0.1"]	{"status": "idle"}	208ASX
-["stag","job15"]	{"platform": "platform15", "args": {"group":"01"}, "enabled": false}	1EX091
-["stag","job16"]	{"platform": "platform16", "args": {"group":"01"}, "enabled": true}	VORM4
+["stag","job15"]	{"platform": "platform15", "direction":"pull", "args": {"group":"01"}, "enabled": false}	1EX091
+["stag","job16"]	{"platform": "platform16", "direction":"pull", "args": {"group":"01"}, "enabled": true}	VORM4
 ["stag","job16","192.168.0.1"]	{"status": "cleaned"}	XYI25
-["stag","job17"]	{"platform": "platform17", "args": {"group":"01"}, "enabled": true}	1LVEOW
+["stag","job17"]	{"platform": "platform17", "direction":"pull", "args": {"group":"01"}, "enabled": true}	1LVEOW
 ["stag","job17","192.168.0.1"]	{"status": "idle"}	1DEQ20
-["stag","job18"]	{"platform": "platform06", "args": {"group":"01"}, "enabled": true}	23SE1K
+["stag","job18"]	{"platform": "platform06", "direction":"pull", "args": {"group":"01"}, "enabled": true}	23SE1K
 ["stag","job18","192.168.0.1"]	{"status":"idle"}	PBG1Y
-["subs","user1","123"] 
     *****/
 
     /** Size & positioning parameters */
@@ -78,29 +77,29 @@ function init(container, width, height) {
             nlset.add(n.id);
         });
         var nodes = [];
+        var status = [];
         rows.forEach(function(row) {
             var key = getKey(row);
             var value = getValue(row);
 
             if(key.length === 3) {
-                // var id = key[0] + '_' + key[1];
-                // if(!nlset.has(id)) {
-                //     nlset.add(id);
-                //     links.push({
-                //         id: id,
-                //         source: key[0],
-                //         target: key[1],
-                //         legend: key[2],
-                //         status: value.status
-                //     });
-                // }
+                var triangleId = key[0] + '_' + key[1] + '_' + key[2];
+                var linkId = key[0] + '_' + key[1];
+                if(!nlset.has(triangleId)) {
+                    nlset.add(triangleId);
+                    status.push({
+                        id: triangleId,
+                        link: linkId,
+                        status: value.status
+                    });
+                }
             } else if(key.length === 2) {
-                var NodeId = value.platform;
+                var nodeId = value.platform;
                 var group = value.args.group;
-                if(!nlset.has(NodeId)) {
-                    nlset.add(NodeId);
+                if(!nlset.has(nodeId)) {
+                    nlset.add(nodeId);
                     nodes.push({
-                        id: NodeId,
+                        id: nodeId,
                         group: group
                     });
                 }
@@ -110,15 +109,16 @@ function init(container, width, height) {
                     links.push({
                         id: linkId,
                         source: key[0],
-                        target: NodeId,
+                        target: nodeId,
                         legend: key[1],
-                        enabled: value.enabled
+                        enabled: value.enabled,
+                        direction: value.direction
                     });
                 }
 
             }
         });
-        return { links: links, nodes: nodes };
+        return { links: links, nodes: nodes, status: status };
     };
     /** End data extraction functions */
 
@@ -267,7 +267,6 @@ function init(container, width, height) {
 
             var nData = [];
             svg.selectAll('.node').each(function (d) {
-                console.log("the d ", d);
                 nData.push(d);
             });
 
@@ -287,27 +286,24 @@ function init(container, width, height) {
                 var y = -r * Math.sin((i + 1) * angle);
                 positions[nData[i].id] = {x: x, y: y};
             }
-            console.log("The positions", positions);
+            // Append center node positions.
+            for(var i = 0; i < centerNodes.length; ++i) {
+                positions[centerNodes[i].id] = centerNodes[i].position;
+            }
 
-            // TODO: Maybe we should translate the g, but this gives more control
             allPoints
                 .transition()
+                .duration(500)
                 .attr('cx', function(d) {
-                    // Interleave smaller radius to allow more nodes.
-                    /*var r = vArcRadius - 2 * nradius * (i % 2);
-                    return -r * Math.cos((i + 1) * angle);*/
                     return positions[d.id].x;
                 })
                 .attr('cy', function(d, i) {
-                    /*
-                    var r = vArcRadius - 2 * nradius * (i % 2);
-                    return -r * Math.sin((i + 1) * angle);
-                    */
                     return positions[d.id].y;
                 });
 
             svg.selectAll('text')
                 .transition()
+                .duration(500)
                 .attr('x', function(d, i) {
                     return positions[d.id].x;
                 })
@@ -332,29 +328,81 @@ function init(container, width, height) {
             var allLinks = svg.selectAll('line');
 
             // Adding connecting links
-            setTimeout(function() {
-                allLinks
-                    .transition()
-                    .attr('x1', function(d) {
-                        var s = document.getElementById(d.source);
-                        return s ? s.cx.baseVal.value : 0;
-                    })
-                    .attr('y1', function(d) {
-                        var s = document.getElementById(d.source);
-                        return s ? s.cy.baseVal.value : 0;
-                    })
-                    .attr('x2', function(d) {
-                        var s = document.getElementById(d.target);
-                        return s ? s.cx.baseVal.value : 0;
-                    })
-                    .attr('y2', function(d) {
-                        var s = document.getElementById(d.target);
-                        return s ? s.cy.baseVal.value : 0;
-                    })
-                    .attr('stroke', function(d) {
-                        return d.enabled ? 'green' : 'lightgrey';
-                    });
-            }, 500);
+            allLinks
+                .transition()
+                .duration(500)
+                .attr('x1', function(d) {
+                    if(!positions[d.source]) { return 0; }
+                    return positions[d.source].x;
+                })
+                .attr('y1', function(d) {
+                    if(!positions[d.source]) { return 0; }
+                    return positions[d.source].y;
+                })
+                .attr('x2', function(d) {
+                    if(!positions[d.target]) { return 0; }
+                    return positions[d.target].x;
+                })
+                .attr('y2', function(d) {
+                    if(!positions[d.target]) { return 0; }
+                    return positions[d.target].y;
+                })
+                .attr('stroke', function(d) {
+                    return d.enabled ? 'green' : 'lightgrey';
+                });
+
+            var linksMid = {};
+            allLinks.each(function(d) {
+                if(!positions[d.source] || !positions[d.target]) {
+                    return;
+                }
+                var midX = 0.5 * (positions[d.source].x + positions[d.target].x);
+                var midY = 0.5 * (positions[d.source].y + positions[d.target].y);
+                var dirX = positions[d.source].x - positions[d.target].x;
+                var dirY = positions[d.source].y - positions[d.target].y;
+                if(d.direction === "pull") {
+                    dirX = -dirX;
+                    dirY = -dirY;
+                }
+                linksMid[d.id] = {mid: {x: midX, y: midY}, direction: {x: dirX, y: dirY}};
+            });
+
+            svg.selectAll('polygon')
+                .data(graph.status, function(d) {
+                    return d.id;
+                })
+                .enter()
+                .append('polygon')
+                .attr('id', function(d) {
+                    return d.id;
+                })
+                .on('mouseover', showTooltip)
+                .on('mousemove', moveTooltip)
+                .on('mouseout', hideTooltip);
+            
+            // TODO: Triangles should be created in origin and then translated.
+            svg.selectAll('polygon')
+                .attr('points', function(d) {
+                    var x = linksMid[d.link].mid.x;
+                    var y = linksMid[d.link].mid.y;
+                    var p1 = x + "," + y;
+                    var p2 = (x - 15) + "," + (y + 45);
+                    var p3 = (x + 15) + "," + (y + 45);
+                    return p1 + " " + p2 + " " + p3;
+                })
+                .attr('transform', function(d) {
+                    var dx = linksMid[d.link].direction.x;
+                    var dy = linksMid[d.link].direction.y;
+                    var angle = -1 * Math.atan2(dx, dy) * 180 / Math.PI;
+                    var x = linksMid[d.link].mid.x;
+                    var y = linksMid[d.link].mid.y;
+                    return "rotate(" + angle + " " + x + " " + y + ")";
+                })
+                .style('stroke', 'black')
+                .style('stroke-width', 3)
+                .style('fill', function(d) {
+                    return colorStatus[d.status];
+                });
         },
         on_resize: resize,
         on_reset: function() {
