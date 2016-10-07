c3f66fdab86a41e896b50abbd36e559c8fc84f4c
Shamis Shukoor <shamis657@gmail.com>
2016-10-07 10:59:29 +0200

    SAML - cert and key config
     * read from config and written to file and cached
     * unaothorized page on authorization error

--- src/dderl_saml_handler.erl
+++ src/dderl_saml_handler.erl
@@ -20,7 +20,8 @@ process_req(<<"POST">>, Req, S = #state{sp = SP}) ->
     case esaml_cowboy:validate_assertion(SP, fun esaml_util:check_dupe_ets/2, Req) of
         {ok, Assertion, RelayState, Req1} ->
             Fun = binary_to_term(base64:decode(http_uri:decode(binary_to_list(RelayState)))),
-            Fun(Req1, Assertion#esaml_assertion.attributes);
+            Fun(Req1, Assertion#esaml_assertion.attributes),
+            {loop, Req1, S, 50000, hibernate};
         {error, Reason, Req2} ->
             {ok, Req3} = cowboy_req:reply(403, [{<<"content-type">>, <<"text/plain">>}],
                 ["Access denied, assertion failed validation:\n", io_lib:format("~p\n", [Reason])],
@@ -37,34 +38,32 @@ info({reply, {saml, UrlSuffix}}, Req, State) ->
             {<<"Location">>, TargetUrl}
         ], <<"Redirecting...">>, Req1),
     {ok, Req2, State};
-info({reply, Body}, Req, State) ->
-    ?Debug("reply ~n~p to ~p", [Body, State]),
-    BodyEnc = if is_binary(Body) -> Body;
-                 true -> imem_json:encode(Body)
-              end,
-    {ok, Req1} =  cowboy_req:reply(200, [
-          {<<"content-encoding">>, <<"utf-8">>}
-        , {<<"content-type">>, <<"application/json">>}
-        ], BodyEnc, Req),
+info({reply, Body}, Req, #state{sp = #esaml_sp{metadata_uri = Url}} = State) ->
+    ?Info("error logging in via saml ~p ", [Body]),
+    TargetUrl = Url ++ string:strip(dderl:get_url_suffix(), both, $/) ++ "/unauthorized.html",
+    {ok, Req1} = cowboy_req:reply(302, [
+            {<<"Cache-Control">>, <<"no-cache">>},
+            {<<"Pragma">>, <<"no-cache">>},
+            {<<"Location">>, TargetUrl}
+        ], <<"Redirecting...">>, Req),
     {ok, Req1, State}.
 
 terminate(_Reason, _Req, _State) -> ok.
 
 initialize(HostUrl, ConsumeUrl) ->
-    % Load the certificate and private key for the SP
-    PrivKey = esaml_util:load_private_key(code:priv_dir("dderl") ++ "/certs/saml.key"),
-    Cert = esaml_util:load_certificate(code:priv_dir("dderl") ++ "/certs/saml.crt"),
-    % io:format("############Cert : ~p~n", [Cert]),
-    % io:format("############PrivKey : ~p~n", [PrivKey]),
-    % We build all of our URLs (in metadata, and in requests) based on this
+    % PrivKey = esaml_util:load_private_key(code:priv_dir("dderl") ++ "/certs/saml.key"),
+    % Cert = esaml_util:load_certificate(code:priv_dir("dderl") ++ "/certs/saml.crt"),
     % Certificate fingerprints to accept from our IDP
+    % Load the certificate and private key for the SP
+    #{cert := _Cert, key := PrivKey} = fetch_cert_key(),
     NewHostUrl = re:replace(HostUrl, ":[0-9]+", "", [{return, list}]) ++ "/",
     NewConsumerUrl = re:replace(ConsumeUrl, ":[0-9]+", "", [{return, list}]),
-    FPs = ["69:D1:0A:C5:C3:AB:FE:A0:E5:D7:15:37:19:D0:34:6B:05:7F:7A:CA:F1:69:61:BC:05:C9:48:3D:1F:72:99:1A"],
+    FPs = ["78:cf:3e:f9:51:1f:d5:d5:e3:5a:88:0e:b5:4b:ee:47:67:ce:94:64"],  %sign cert
+    % FPs = ["80:d1:54:b6:58:fd:34:96:23:93:39:76:e0:00:5a:f2:96:98:ab:5a"],  %saml cert
 
     SP = esaml_sp:setup(#esaml_sp{
         key = PrivKey,
-        %certificate = Cert,
+        % certificate = Cert,
         sp_sign_requests = false,
         trusted_fingerprints = FPs,
         consume_uri = NewConsumerUrl,
@@ -83,3 +82,62 @@ fwdUrl(HostUrl, ConsumeUrl, RelayStateCbFun) when is_function(RelayStateCbFun) -
 fwdUrl(HostUrl, ConsumeUrl, RelayState) when is_binary(RelayState) ->
     {SP, #esaml_idp_metadata{login_location = IDP}} = initialize(HostUrl, ConsumeUrl),
     esaml_binding:encode_http_post(IDP, SP:generate_authn_request(IDP), RelayState).
+
+fetch_cert_key() ->
+    case imem_cache:read({?MODULE, samlSslOpts}) of
+        [] -> fetch_cert_key(?SAMLSSLOPTS);
+        [CertKey] -> CertKey
+    end.
+
+fetch_cert_key('$no_ssl_conf') ->
+    {CertBin, KeyBin} = 
+    case file:read_file(priv_cert_file("saml.crt")) of
+        {ok, CertB} -> 
+            case file:read_file(priv_cert_file("saml.key")) of
+                {ok, KeyB} -> {CertB, KeyB};
+                _ -> error("SAML : Private key is not available")
+            end;
+        _ -> error("SAML : Certificate is not available")
+    end,
+    [{cert, Cert}] = imem_server:get_cert_key(CertBin),
+    Key = get_priv_key(KeyBin),
+    SamlSslCache = #{cert => Cert, key => Key},
+    SamlSslConfig = #{cert => CertBin, key => KeyBin},
+    ?Info("Installing SAML SSL ~p", [SamlSslConfig]),
+    ?PUT_CONFIG(samlSslOpts, [], SamlSslConfig,
+                list_to_binary(
+                  io_lib:format(
+                    "Installed at ~p on ~s",
+                    [node(), imem_datatype:timestamp_to_io(
+                               os:timestamp())]))),
+    imem_cache:write({?MODULE, samlSslOpts}, SamlSslCache),
+    SamlSslCache;
+fetch_cert_key(#{cert := CertBin, key := KeyBin}) ->
+    CertFile = priv_cert_file("saml.crt"),
+    KeyFile = priv_cert_file("saml.key"),
+    case file:read_file(CertFile) of
+        {ok, CertBin} -> nop;
+        _ -> ok = file:write_file(CertFile, CertBin)
+    end,
+    case file:read_file(KeyFile) of
+        {ok, KeyBin} -> nop;
+        _ -> ok = file:write_file(KeyFile, KeyBin)
+    end,
+    [{cert, Cert}] = imem_server:get_cert_key(CertBin),
+    Key = get_priv_key(KeyBin),
+    SamlSsl = #{cert => Cert, key => Key},
+    imem_cache:write({?MODULE, samlSslOpts}, SamlSsl),
+    SamlSsl.
+    
+priv_cert_file(FileName) -> filename:join([code:priv_dir("dderl"), "certs", FileName]).
+
+get_priv_key(KeyBin) ->
+    [KeyEntry] = public_key:pem_decode(KeyBin),
+    case public_key:pem_entry_decode(KeyEntry) of
+        #'PrivateKeyInfo'{privateKey = KeyData} ->
+            KeyDataBin = if is_list(KeyData) -> list_to_binary(KeyData);
+                            true -> KeyData
+                         end,
+            public_key:der_decode('RSAPrivateKey', KeyDataBin);
+        Other -> Other
+    end.
