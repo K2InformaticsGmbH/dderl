9beb0fe84db77fd27f6178bc832a23c06bac7dfc
Agustin Cautin <acautin@gmail.com>
2016-10-18 13:37:53 +0200

    Cancel session timer if it was set before creating a new one to avoid race conditions.

--- src/dderl_session.erl
+++ src/dderl_session.erl
@@ -88,7 +88,7 @@ process_request(Adapter, Type, Body, ReplyPid, RemoteEp, DderlSess) ->
 
 init([ConnInfo]) ->
     process_flag(trap_exit, true),
-    {ok, TRef} = timer:send_after(?SESSION_IDLE_TIMEOUT, die),
+    TRef = erlang:send_after(?SESSION_IDLE_TIMEOUT, self(), die),
     case erlimem:open({rpc, node()}, imem_meta:schema()) of
         {error, Error} ->
             ?Error("erlimem open error : ~p", [Error]),
@@ -105,7 +105,7 @@ handle_call(Unknown, _From, #state{user=_User}=State) ->
     {reply, {not_supported, Unknown} , State}.
 
 handle_cast({process, Adapter, Typ, WReq, From, RemoteEp}, #state{tref=TRef} = State) ->
-    timer:cancel(TRef),
+    cancel_timer(TRef),
     State0 = try process_call({Typ, WReq}, Adapter, From, RemoteEp, State)
     catch Class:Error ->
             ?Error("Problem processing command: ~p:~p~n~p~n", [Class, Error, erlang:get_stacktrace()]),
@@ -117,8 +117,9 @@ handle_cast(_Unknown, #state{user=_User}=State) ->
     ?Error("~p received unknown cast ~p for ~p", [self(), _Unknown, _User]),
     {noreply, State}.
 
-handle_info(rearm_session_idle_timer, State) ->
-    {ok, NewTRef} = timer:send_after(?SESSION_IDLE_TIMEOUT, die),
+handle_info(rearm_session_idle_timer, #state{tref=TRef} = State) ->
+    cancel_timer(TRef),
+    NewTRef = erlang:send_after(?SESSION_IDLE_TIMEOUT, self(), die),
     {noreply, State#state{tref=NewTRef}};
 handle_info(die, #state{user=User}=State) ->
     ?Info([{user, User}], "session ~p idle for ~p ms", [{self(), User}, ?SESSION_IDLE_TIMEOUT]),
@@ -558,7 +559,9 @@ process_call({Cmd, ReqData}, Adapter, From, {SrcIp,_},
     CurrentPriv = Adapter:add_conn_info(proplists:get_value(Adapter, AdaptPriv), ConnInfo),
     NewCurrentPriv =
         try
-            Adapter:process_cmd({Cmd, BodyJson, Id}, Sess, UserId, From, CurrentPriv, self())
+            TmpPriv = Adapter:process_cmd({Cmd, BodyJson, Id}, Sess, UserId, From, CurrentPriv, self()),
+            self() ! rearm_session_idle_timer,
+            TmpPriv            
         catch Class:Error ->
                 ?Error("Problem processing command: ~p:~p~n~p~n", [Class, Error, erlang:get_stacktrace()]),
                 reply(From, [{<<"error">>, <<"Unable to process the request">>}], self()),
@@ -679,3 +682,9 @@ produce_buffer_csv_rows({Rows, Continuation}, From, TableId, RowFun, ColumnPosit
     CsvRows = iolist_to_binary(add_csv_separators(ExpandedRows, ColSep, RowSep)),
     From ! {reply_csv, <<>>, CsvRows, continue},
     produce_buffer_csv_rows(ets:select(Continuation), From, TableId, RowFun, ColumnPositions, ColSep, RowSep).
+
+-spec cancel_timer(undefined | reference()) -> ok.
+cancel_timer(undefined) -> ok;
+cancel_timer(TRef) ->
+    erlang:cancel_timer(TRef),
+    ok.
