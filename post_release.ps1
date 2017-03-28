62f32f40085ff94b0d62b877e43a010c4a4dea54
Bikram Chatterjee <razorpeak@gmail.com>
2017-03-28 14:42:29 +0200

    public is already deleted by 'run build'; delete only dev for cleanup

--- post_release.ps1
+++ post_release.ps1
@@ -15,21 +15,14 @@ If (Test-Path node_modules) {
 Write-Host "===> npm install"
 npm install
 
-If (Test-Path public) {
-    Remove-Item public -Force -Recurse
-    Write-Host "===> public dir deleted"
-}
 Write-Host "===> npm run build"
 npm run build
 
-Write-Host "===> clean up" -foregroundcolor "green"
-$delFiles = "d3_*","node_modules","static","test",`
-            "karma.conf.js",".jshintrc*","package.json*"
-For($i = 0; $i -lt $delFiles.Length; $i++) {
-    If (Test-Path $delFiles[$i]) {
-        Remove-Item $delFiles[$i] -Force -Recurse
-        Write-Host "===> "$delFiles[$i]" deleted"
-    } Else {
-        Write-Host "===> "$delFiles[$i]" not found!"
-    }
+cd ..
+Write-Host "===> clean up"
+If (Test-Path "dev") {
+    Remove-Item "dev" -Force -Recurse
+    Write-Host "===> dir 'dev' deleted"
+} Else {
+    Write-Host "===> dir 'dev' not found!"
 }
