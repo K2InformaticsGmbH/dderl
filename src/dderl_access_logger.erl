e8944cc4cc6316821c10048970cdb97148e28432
Bikram Chatterjee <razorpeak@gmail.com>
2017-04-27 14:38:40 +0200

    local function names simplified, interface extended for re use

--- src/dderl_access_logger.erl
+++ src/dderl_access_logger.erl
@@ -1,32 +1,32 @@
 -module(dderl_access_logger).
 -include("dderl.hrl").
 
--export([install/1, uninstall/1, log/2]).
+-export([install/1, install/2, uninstall/1, log/2]).
 
 -define(AccessSchema,
-        [{src,       fun access_src/1},
-         {proxy,     fun access_proxy/1},
-         {userId,    fun access_userid/1},
-         {userName,  fun access_username/1},
-         {sessId,    fun access_sessionid/1},
-         {version,   fun access_version/1},
+        [{src,       fun src/1},
+         {proxy,     fun proxy/1},
+         {userId,    fun userid/1},
+         {userName,  fun username/1},
+         {sessId,    fun sessionid/1},
+         {version,   fun version/1},
          loglevel,
          cmd,
-         {args,      fun access_args/1},
-         {bytes,     fun access_bytes/1},
-         {time,      fun access_time/1},
+         {args,      fun args/1},
+         {bytes,     fun bytes/1},
+         {time,      fun time/1},
          connUser,
          connTarget,
          connDbType,
          connStr,
-         {sql,       fun access_sql/1}]).
+         {sql,       fun sql/1}]).
 
-
-install(App) ->
+install(App) -> install(App, ?AccessSchema).
+install(App, AccessSchema) ->
     ok = gen_event:add_handler(
            lager_event, {dderl_access_lager_file_backend, App},
            [{file, "log/dderl_access.log"}, {level, debug}, {size, 10485760},
-            {date, "$D0"}, {count, 5}, {application, App}, {props, ?AccessSchema}]),
+            {date, "$D0"}, {count, 5}, {application, App}, {props, AccessSchema}]),
     ok = lager:set_loglevel({dderl_access_lager_file_backend, App}, debug),
     ?Info("activity logger started").
 
@@ -34,47 +34,47 @@ uninstall(App) ->
     ok = gen_event:delete_handler(
            lager_event, {dderl_access_lager_file_backend, App}, []).
 
-access_src(Access) ->
+src(Access) ->
     case maps:get(src, Access, "") of
         SrcIp when is_tuple(SrcIp) -> inet:ntoa(SrcIp);
         SrcIp when is_list(SrcIp) -> SrcIp;
         SrcIp -> io_lib:format("~p", [SrcIp])
     end.
 
-access_proxy(Access) ->
+proxy(Access) ->
     SrcIp = maps:get(src, Access, ""),
     case maps:get(proxy, Access, "") of
         SrcIp -> "yes";
         _ -> "no"
     end.
 
-access_userid(Access) ->
+userid(Access) ->
     case maps:get(userId, Access, "") of
         undefined -> "";
         User when is_list(User) -> User;
         User -> io_lib:format("~p", [User])
     end.
 
-access_username(Access) ->
+username(Access) ->
     case maps:get(userName, Access, "") of
         User when is_list(User) -> User;
         User when is_binary(User) -> binary_to_list(User);
         User -> io_lib:format("~p", [User])
     end.
 
-access_sessionid(Access) ->
+sessionid(Access) ->
     base64:encode_to_string(
       integer_to_list(
         erlang:phash2(
           maps:get(sessId, Access, "")))).
 
-access_version(Access) ->
+version(Access) ->
     case application:get_key(maps:get(app,Access,undefined)) of
         {ok, Vsn} -> Vsn;
         _ -> ""
     end.
 
-access_args(Access) ->
+args(Access) ->
     CmdArgs = maps:get(args, Access, ""),
     binary_to_list(
       case CmdArgs of
@@ -93,14 +93,14 @@ access_args(Access) ->
           CmdArgs -> list_to_binary(io_lib:format("~p", [CmdArgs]))
       end).
 
-access_bytes(Access) ->
+bytes(Access) ->
     case maps:get(bytes, Access, "") of
         "" -> "";
         Bytes when is_integer(Bytes) -> integer_to_list(Bytes);
         Bytes -> io_lib:format("~p", [Bytes])
     end.
 
-access_time(Access) ->
+time(Access) ->
     case maps:get(time, Access, "") of
         "" -> "";
         Time when is_integer(Time) -> integer_to_list(Time);
@@ -108,7 +108,7 @@ access_time(Access) ->
     end.
 
 
-access_sql(Access) ->
+sql(Access) ->
     case maps:get(args, Access, "") of
         #{<<"qstr">> := QStr} ->
             re:replace(
