bbf1fb9c61852771d4adbb7377269233719a32cf
Bikram Chatterjee <razorpeak@gmail.com>
2017-05-15 16:03:20 +0200

    MSI build plugin WIP

--- post_release.ps1
+++ post_release.ps1
@@ -1,4 +1,4 @@
-cd _build/default/rel/dderl/lib
+cd _build/prod/rel/dderl/lib
 
 $source = Get-ChildItem -Filter imem-* |
           Select-Object -First 1 -Expand FullName
@@ -10,13 +10,13 @@ cd dderl-*/priv/dev
 
 If (Test-Path node_modules) {
     Remove-Item node_modules -Force -Recurse
-    Write-Host "===> directory 'node_modules' deleted"
+    Write-Host "===> directory 'node_modules' deleted" -foregroundcolor "magenta"
 }
 
-Write-Host "===> npm install"
+Write-Host "===> npm install" -foregroundcolor "magenta"
 npm install
 
-Write-Host "===> npm run build"
+Write-Host "===> npm run build" -foregroundcolor "magenta"
 npm run build
 
 cd ..
@@ -25,20 +25,20 @@ $Path = "dev"
 If (Test-Path $Path) {
     If (!(Get-ItemProperty $Path).Target) {
         Remove-Item $Path -Force -Recurse
-        Write-Host "===> directory '" -nonewline
-        Write-Host $Path -nonewline
-        Write-Host "' deleted"
+        Write-Host "===> directory '" -nonewline -foregroundcolor "magenta"
+        Write-Host $Path -nonewline -foregroundcolor "magenta"
+        Write-Host "' deleted" -foregroundcolor "magenta"
     } Else {
-        Write-Host "===> directory '" -nonewline
-        Write-Host $Path -nonewline
-        Write-Host "' is a symbolic link - no cleanup"
+        Write-Host "===> directory '" -nonewline -foregroundcolor "magenta"
+        Write-Host $Path -nonewline -foregroundcolor "magenta"
+        Write-Host "' is a symbolic link - no cleanup" -foregroundcolor "magenta"
         If (Test-Path dev/node_modules) {
             Remove-Item dev/node_modules -Force -Recurse
-            Write-Host "===> directory 'dev/node_modules' deleted"
+            Write-Host "===> directory 'dev/node_modules' deleted" -foregroundcolor "magenta"
         }
     }
 } Else {
-        Write-Host "===> directory '" -nonewline
-        Write-Host $Path -nonewline
-        Write-Host "' not found"
+        Write-Host "===> directory '" -nonewline -foregroundcolor "magenta"
+        Write-Host $Path -nonewline -foregroundcolor "magenta"
+        Write-Host "' not found" -foregroundcolor "magenta"
 }
