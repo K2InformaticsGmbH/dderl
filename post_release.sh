25efcad23ac30ba25682ee741a8a40c7cffbb51c
Bikram Chatterjee <razorpeak@gmail.com>
2017-09-07 13:05:44 +0200

    cosmetics

--- post_release.sh
+++ post_release.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
-cd _build/prod/rel/dderl/lib
+cd _build/prod/rel/$1/lib
 mv imem-*/ebin/filename.beam stdlib-*/ebin/
+echo "===> replaced stdlib-*/ebin/filename.beam"
 
 cd dderl-*/priv/dev
 rm -rf node_modules
+echo "===> dir 'node_modules' deleted"
+
+echo "===> npm install"
 npm install
+
+echo "===> npm run build"
 npm run build
 
 cd ..
+echo "===> dir 'dev' deleted"
 rm -rf dev
