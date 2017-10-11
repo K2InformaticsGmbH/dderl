7a2593616f31c94773cb5fe6afb167b3efd83474
shamis <shamis657@gmail.com>
2017-10-11 10:12:06 +0000

    ctrl + enter to execute query #467

--- priv/dev/static/scripts/dderl.sql.js
+++ priv/dev/static/scripts/dderl.sql.js
@@ -104,7 +104,7 @@ function insertAtCursor(myField, myValue) {
                        'Execute fetch tail mode'    : { typ : 'btn', icn : 'step-forward ellipsis', clk : '_toolBarTblFetchNTail'   },
                        'Execute tail mode only'     : { typ : 'btn', icn : 'fetch-only ellipsis',   clk : '_toolBarTblTailOnly'     },
                        ''                           : { typ : 'sel',                                clk : '_toolBarChangeSql'       }},
-
+    
     // These options will be used as defaults
     options: {
         // dialog options default override
@@ -357,7 +357,7 @@ function insertAtCursor(myField, myValue) {
         // need the max footer with to set as dlg minWidth
         self._createDlgFooter();
         self._createDlg();
-
+        self._addKeyEventHandlers();
         self._createContextMenus();
 
         // setting up the event handlers last to aid debugging
@@ -689,6 +689,20 @@ function insertAtCursor(myField, myValue) {
             .css('overflow', 'hidden');
 
         self._footerWidth = self._addBtngrpToDiv(self._footerDiv);
+    },
+
+    _addKeyEventHandlers: function() {
+        var self = this;
+        this.element.keydown(function(e) {
+            var c = e.keyCode;
+            var ctrlDown = e.ctrlKey || e.metaKey;
+            // Check for ctrl+enter
+            if(ctrlDown && c === 13) {
+                console.log("ctrl+enter detected");
+                self._toolBarTblReload();
+            }
+        });
+              
     },
 
     _setTitle: function(newTitle) {
@@ -1300,6 +1314,7 @@ function insertAtCursor(myField, myValue) {
         this._editDiv.remove();
         this._footerDiv.remove();
         this.element.removeAttr('style class scrolltop scrollleft');
+        this.element.off("keydown");
     },
 
   });
