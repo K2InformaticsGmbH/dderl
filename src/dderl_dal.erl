d382b818c3a74c2a3b63d1010cd55e7ea0bba5a8
Bikram Chatterjee <razorpeak@gmail.com>
2017-11-08 17:16:51 +0100

    version 1.9.2, cosmetics

--- src/dderl_dal.erl
+++ src/dderl_dal.erl
@@ -139,7 +139,7 @@ user_name(Id) when is_integer(Id) ->
 -spec add_view({atom(), pid()} | undefined, ddEntityId(), binary(), ddEntityId(), #viewstate{}) -> ddEntityId().
 add_view(undefined, Owner, Name, CmdId, ViewsState) ->
     gen_server:call(?MODULE, {add_view, Owner, Name, CmdId, ViewsState});
-add_view(Sess, Owner, Name, CmdId, ViewsState) -> 
+add_view(Sess, Owner, Name, CmdId, ViewsState) ->
     Id = case Sess:run_cmd(select, [ddView, [{#ddView{name=Name, cmd = CmdId, id='$1', owner=Owner, _='_'}
                                             , []
                                             , ['$1']}]]) of
@@ -271,7 +271,7 @@ get_view(Sess, ViewId) ->
 
 -spec get_view({atom(), pid()} | undefined, binary(), atom(), ddEntityId() | '_') -> #ddView{} | undefined.
 get_view(undefined, Name, Adapter, Owner) -> gen_server:call(?MODULE, {get_view, Name, Adapter, Owner});
-get_view(Sess, Name, Adapter, Owner) -> 
+get_view(Sess, Name, Adapter, Owner) ->
     ?Debug("get_view ~p", [Name]),
     case check_cmd_select(Sess, [ddView,[{#ddView{name=Name, owner=Owner, _='_'}, [], ['$_']}]]) of
         {error, _} = Error -> Error;
@@ -733,7 +733,7 @@ get_restartable_apps() ->
                       connInfo => map(),
                       stateUpdateUsr =>  fun((any(), any()) -> any()),
                       stateUpdateSKey => fun((any(), any()) -> any()),
-                      relayState => fun((any(), any()) -> any()), 
+                      relayState => fun((any(), any()) -> any()),
                       urlPrefix => list()}) -> ok | map().
 process_login(#{<<"smsott">> := Token} = Body, State,
               #{auth := AuthFun} = Ctx) ->
@@ -817,7 +817,7 @@ expand_rows([FullRowTuple | RestRows], TableId, RowFun, ColumnPos) ->
 is_proxy(AppId, NetCtx) ->
     ProxyCheckFun = ?GET_CONFIG(isProxyCheckFun, AppId, <<"fun(NetCtx) -> false end">>, "Function checks if user is coming through a proxy or not"),
     CacheKey = {?MODULE, isProxyCheckFun, ProxyCheckFun},
-    case imem_cache:read(CacheKey) of 
+    case imem_cache:read(CacheKey) of
         [] ->
             case imem_datatype:io_to_fun(ProxyCheckFun) of
                 PF when is_function(PF, 1) ->
@@ -826,7 +826,7 @@ is_proxy(AppId, NetCtx) ->
                 _ ->
                     ?Error("Not a valid is proxy fun configured"),
                     false
-            end;    
+            end;
         [PF] when is_function(PF, 1) -> exec_is_proxy_fun(PF, NetCtx);
         _ -> false
     end.
