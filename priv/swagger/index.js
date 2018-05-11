6fb858c8a92eac604a80022cea997735ab56e462
Bikram Chatterjee <razorpeak@gmail.com>
2018-05-11 07:59:47 +0200

    flex styled

--- priv/swagger/index.js
+++ priv/swagger/index.js
@@ -10,6 +10,9 @@ $(function () {
             brand = JSON.parse(brand);
         $('#brand_title').text(brand.title);
         $('#brand_banner').attr('href', brand.url);
+        $('#brand_image').one('load', function () {
+            $('#k2logo').css('height', $('#brand_image').height());
+        });
         $('#brand_image').attr('src', brand.logo);
         SwaggerUI({
             dom_id: '#SwaggerUI',
