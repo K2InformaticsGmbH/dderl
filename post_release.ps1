bf0bbacc8536b2f36e7c9c13419e6fad81ba211c
Bikram Chatterjee <razorpeak@gmail.com>
2018-05-12 10:45:16 +0200

    windows reelase tested

--- post_release.ps1
+++ post_release.ps1
@@ -1,6 +1,7 @@
 Param([string]$app="dderl")
 
-cd _build/prod/rel/$app/
+$root = (Resolve-Path "_build/prod/rel/$app/").Path
+cd $root
 $erts = Get-ChildItem -Filter erts-* |
         Select-Object -First 1 -Expand FullName
 $ertsIni = "$erts\bin\erl.ini"
@@ -12,47 +13,73 @@ Else {
     Write-Host "===> not found $ertsIni" -foregroundcolor "red"
 }
 
-$dderlDev = "lib/dderl-*/priv/dev"
+$dderlPriv = (Resolve-Path "$root\lib\dderl-*\priv").Path
+Write-Host "===> building dderl @ $dderlPriv ..." -foregroundcolor "magenta"
 
-If (Test-Path $dderlDev) {
-    cd $dderlDev
-    Write-Host "===> building dderl-npm..." -foregroundcolor "magenta"
+If (Test-Path "$dderlPriv\dev") {
+    cd "$dderlPriv\dev"
+    Write-Host "===> building dderl (dev)@ $dderlPriv ..." -foregroundcolor "magenta"
 } Else {
-    Write-Host "===> dderl-npm already built!" -foregroundcolor "magenta"
+    Write-Host "===> dderl (dev) already built!" -foregroundcolor "magenta"
+    exit
+}
+
+If (Test-Path node_modules) {
+    # Remove-Item node_modules -Force -Recurse
+    Write-Host "===> directory 'dev\node_modules' deleted" -foregroundcolor "magenta"
+}
+
+Write-Host "===> npm install (dev)" -foregroundcolor "magenta"
+# npm install
+
+If (Test-Path "$dderlPriv\swagger") {
+    cd "$dderlPriv\swagger"
+    Write-Host "===> building dderl (swagger) @ $dderlPriv ..." -foregroundcolor "magenta"
+} Else {
+    Write-Host "===> dderl (swagger) already built!" -foregroundcolor "magenta"
     exit
 }
 
 If (Test-Path node_modules) {
     Remove-Item node_modules -Force -Recurse
-    Write-Host "===> directory 'node_modules' deleted" -foregroundcolor "magenta"
+    Write-Host "===> directory 'swagger\node_modules' deleted" -foregroundcolor "magenta"
 }
 
-Write-Host "===> npm install" -foregroundcolor "magenta"
+Write-Host "===> npm install (swagger)" -foregroundcolor "magenta"
 npm install
 
+cd "$dderlPriv\dev"
 Write-Host "===> npm run build-prod" -foregroundcolor "magenta"
-npm run build
+npm run build-prod
 
-cd ..
-Write-Host "===> clean up"
-$Path = "dev"
-If (Test-Path $Path) {
-    If (!(Get-ItemProperty $Path).Target) {
-        Remove-Item $Path -Force -Recurse
-        Write-Host "===> directory '" -nonewline -foregroundcolor "magenta"
-        Write-Host $Path -nonewline -foregroundcolor "magenta"
-        Write-Host "' deleted" -foregroundcolor "magenta"
+Write-Host "===> clean up dev"
+If (Test-Path "$dderlPriv\dev") {
+    If (!(Get-ItemProperty "$dderlPriv\dev").Target) {
+        Remove-Item "$dderlPriv\dev" -Force -Recurse
+        Write-Host "===> directory $dderlPriv\dev deleted" -foregroundcolor "magenta"
     } Else {
-        Write-Host "===> directory '" -nonewline -foregroundcolor "magenta"
-        Write-Host $Path -nonewline -foregroundcolor "magenta"
-        Write-Host "' is a symbolic link - no cleanup" -foregroundcolor "magenta"
-        If (Test-Path dev/node_modules) {
-            Remove-Item dev/node_modules -Force -Recurse
-            Write-Host "===> directory 'dev/node_modules' deleted" -foregroundcolor "magenta"
+        Write-Host "===> directory $dderlPriv\dev is a symbolic link - no cleanup" -foregroundcolor "magenta"
+        If (Test-Path "$dderlPriv\dev\node_modules") {
+            Remove-Item "$dderlPriv\dev\node_modules" -Force -Recurse
+            Write-Host "===> directory 'dev\node_modules' deleted" -foregroundcolor "magenta"
         }
     }
 } Else {
-        Write-Host "===> directory '" -nonewline -foregroundcolor "magenta"
-        Write-Host $Path -nonewline -foregroundcolor "magenta"
-        Write-Host "' not found" -foregroundcolor "magenta"
+        Write-Host "===> directory $dderlPriv\dev not found" -foregroundcolor "magenta"
+}
+
+Write-Host "===> clean up swagger"
+If (Test-Path "$dderlPriv\swagger") {
+    If (!(Get-ItemProperty "$dderlPriv\swagger").Target) {
+        Remove-Item "$dderlPriv\swagger" -Force -Recurse
+        Write-Host "===> directory $dderlPriv\swagger deleted" -foregroundcolor "magenta"
+    } Else {
+        Write-Host "===> directory $dderlPriv\swagger is a symbolic link - no cleanup" -foregroundcolor "magenta"
+        If (Test-Path "$dderlPriv\swagger\node_modules") {
+            Remove-Item "$dderlPriv\swagger\node_modules" -Force -Recurse
+            Write-Host "===> directory 'swagger\node_modules' deleted" -foregroundcolor "magenta"
+        }
+    }
+} Else {
+        Write-Host "===> directory $dderlPriv\swagger not found" -foregroundcolor "magenta"
 }
--- post_release.sh
+++ post_release.sh
@@ -1,22 +1,40 @@
 #!/bin/sh
-echo "===> dderl post_release"
-
-dderlDev=_build/prod/rel/$1/lib/dderl-*/priv/dev
-if [ -d $dderlDev ]; then
-    cd $dderlDev
-    rm -rf node_modules
-    echo "===> dir 'node_modules' deleted"
+if [ -z "$1" ]
+then
+    app="dderl"
 else
-    echo "===> dderl-npm already built!"
+    app="$1"
+fi
+echo "===> post_release $1"
+
+dderlPriv=$(readlink -f _build/prod/rel/$1/lib/dderl-*/priv/)
+if [ -d "$dderlPriv/dev" ]; then
+    cd $dderlPriv/dev
+    rm -rf node_modules
+    echo "===> dir 'dev/node_modules' deleted"
+else
+    echo "===> dderl-dev already built!"
     exit 0
 fi
 
 echo "===> npm install"
 npm install
 
-echo "===> npm run build-prod"
-npm run build
+if [ -d "$dderlPriv/swagger" ]; then
+    cd $dderlPriv/swagger
+    rm -rf node_modules
+    echo "===> dir 'swagger/node_modules' deleted"
+else
+    echo "===> dderl-swagger already built!"
+    exit 0
+fi
 
-cd ..
+echo "===> npm run build-prod"
+cd $dderlPriv/dev
+npm run build-prod
+
+rm -rf $dderlPriv/dev
 echo "===> dir 'dev' deleted"
-rm -rf dev
+
+rm -rf $dderlPriv/swagger
+echo "===> dir 'swagger' deleted"
